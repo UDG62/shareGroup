05b044a4c9c07af4d4f29e5a0214ced80397f3b8
Louis <louishanne18@gmail.com>
2024-10-16 10:16:39 +0200

    Chapitre3

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,6 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ContactType;
+use App\Form\CategorieType;
+use App\Entity\Contact;
+use App\Entity\Categorie;
+
+
 
 class BaseController extends AbstractController
 {
@@ -14,22 +22,58 @@ class BaseController extends AbstractController
         return $this->render('base/index.html.twig', [
         ]);
     }
+
     #[Route('/contact', name: 'app_contact')]
-    public function contact(): Response
+    public function contact(Request $request, EntityManagerInterface $em): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em->persist($contact);
+                $em->flush();
+                $this->addFlash('notice','Message envoyé');
+                return $this->redirectToRoute('app_contact');
+            }
+            }
         return $this->render('base/contact.html.twig', [
+            'form' => $form->createView()
         ]);
     }
+
     #[Route('/Apropos', name: 'app_a_propos')]
     public function apropos(): Response
     {
         return $this->render('base/apropos.html.twig', [
         ]);
     }
+
     #[Route('/mentions', name: 'app_mentions')]
     public function mentions(): Response
     {
         return $this->render('base/mentions.html.twig', [
+        ]);
+    }
+
+    #[Route('/categorie', name: 'app_categorie')]
+    public function categorie(Request $request, EntityManagerInterface $em): Response
+    {
+        $categorie = new Categorie();
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em->persist($categorie);
+                $em->flush();
+                $this->addFlash('notice','Catégorie envoyé');
+                return $this->redirectToRoute('app_categorie');
+            }
+            }
+        return $this->render('base/categorie.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
